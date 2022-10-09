27625416622a1fe86ffa907b05200b1c143294ec
eisandbar <eisandbar@gmail.com>
2022-10-08 21:55:27 -0400

    refactor: Use background component in main
    Changed main to use background component as we removed it from index.html

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,20 +1,25 @@
 import React, { ReactElement } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Projects } from "./Pages/Projects";
 import { Languages } from "./Pages/Languages";
 import { Home } from "./Pages/Home";
+import { BackGround } from "./Cards/Background";
 // import "./index.css";
 
 const Routing: React.FC = (): ReactElement => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/lang" element={<Languages />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <BackGround>
+      <Router>
+        <Routes>
+          <Route path="/lang" element={<Languages />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </BackGround>
   );
 };
 
