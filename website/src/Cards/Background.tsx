714c2b38150c87b46d17eac160bf34d961c77252
eisandbar <eisandbar@gmail.com>
2022-10-08 21:48:32 -0400

    refactor: Move background image
    To fix loading issues background moved background image from index html
    to a separate react component

---
+++ website/src/Cards/Background.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from "react";
+
+interface BackGroundProps {
+  children: React.ReactNode;
+}
+
+export const BackGround: React.FC<BackGroundProps> = (
+  props: BackGroundProps
+): ReactElement => {
+  return (
+    <div
+      className="bg-image"
+      style={{
+        backgroundImage: "url(../img/backgroundSky.jpg)",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",
+        overflow: "auto",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
