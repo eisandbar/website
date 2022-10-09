80870ce6800e57c77c82b9118cb4372730d9da09
eisandbar <eisandbar@gmail.com>
2022-10-09 02:23:30 -0400

    feat: Add image with text component
    Added component where text is overlayed over image

--- website/src/Components/BaseImg.tsx
+++ website/src/Components/BaseImg.tsx
@@ -1,20 +1,21 @@
 import React, { ReactElement } from "react";
 import { Image } from "react-bootstrap";
 
-export const BaseImg = (props: {
+interface ImageProps {
   setOpen: React.Dispatch<React.SetStateAction<string>>;
   open: string;
   name: string;
   src: string;
-}): ReactElement => {
+}
+
+export const BaseImg: React.FC<ImageProps> = (
+  props: ImageProps
+): ReactElement => {
   return (
     <Image
       src={props.src}
       fluid
-      style={{
-        height: "8rem",
-        width: "8rem",
-      }}
+      className="imageBox"
       onClick={() => {
         if (props.open === props.name) {
           props.setOpen("");
@@ -23,5 +24,32 @@ export const BaseImg = (props: {
         }
       }}
     />
+  );
+};
+
+export const BaseImgWithText: React.FC<ImageProps> = (
+  props: ImageProps
+): ReactElement => {
+  const image =
+    "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(" +
+    props.src +
+    ")";
+
+  return (
+    <div
+      className="bg-image imageBox rounded-4"
+      style={{
+        backgroundImage: image,
+      }}
+      onClick={() => {
+        if (props.open === props.name) {
+          props.setOpen("");
+        } else {
+          props.setOpen(props.name);
+        }
+      }}
+    >
+      <h4 className="text-light">{props.name}</h4>
+    </div>
   );
 };
