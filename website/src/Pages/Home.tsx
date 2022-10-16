dae209e0c3a4b4d6ea7cd5145b2b4304c5c41c04
eisandbar <eisandbar@gmail.com>
2022-10-15 22:31:03 -0400

    feat(Home): Add intro animation
    Added a simple animation when loading the page

--- website/src/Pages/Home.tsx
+++ website/src/Pages/Home.tsx
@@ -1,16 +1,29 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { NavBar } from "../Components/Nav";
 import Row from "react-bootstrap/Row";
 import { HomeCard, HomeTitle } from "../Components/Home";
-export const Home: React.FC = (): ReactElement => {
+
+export const Home = (): ReactElement => {
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [height, setHeight] = useState("100%");
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 500);
+    setTimeout(() => {
+      setHeight("20rem");
+      setShow2(true);
+    }, 1500);
+  });
+
   return (
-    <div className="Home">
-      <NavBar />
-      <Row className="align-items-center" style={{ height: "20rem" }}>
-        <HomeTitle />
+    <div className="Home h-100" hidden={!show}>
+      <NavBar show={show2} />
+      <Row className="align-items-center intro" style={{ height }}>
+        <HomeTitle show={show} />
       </Row>
       <Row>
-        <HomeCard />
+        <HomeCard show={show2} />
       </Row>
     </div>
   );
